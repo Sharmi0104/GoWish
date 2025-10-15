46c0e2f739e8eac49ad108ed4e0b86efd0285d08
Sharmila Panaym <sh.panyam@gmail.com>
2025-10-15 15:21:07 +0200

    Loginpage

---
+++ tests/pages/loginPage.ts
@@ -0,0 +1,116 @@
+import { Page, expect, Locator } from "@playwright/test";
+export class loginPage {
+  readonly page: Page; 
+  readonly emailInput: Locator;
+  readonly passwordInput: Locator;
+  readonly loginButton: Locator;
+  readonly userActionsContainer: Locator;
+  readonly userName: Locator;
+  readonly createUserButton: Locator;
+
+  constructor(page: Page) {
+    this.page = page;
+    this.emailInput = this.page.locator('input[data-cy="signupEmailInput"]');
+    this.passwordInput = this.page.locator(
+      'input[data-testid="loginPasswordInput"]'
+    );
+    this.loginButton = this.page.locator(
+      'button[data-cy="registerNameNextButton"]'
+    );
+    this.userActionsContainer = page.locator(
+      "div.UserHeader__UserActionsContainer-sc-6440e44-5"
+    );
+    this.userName = this.userActionsContainer.locator(
+      "div.UserHeader__Name-sc-6440e44-2"
+    );
+    this.createUserButton = page.locator("button", { hasText: "Opret bruger" });
+  }
+  async handlecokkies() {
+    const dismissOverlay = async () => {
+      const cookieBanner = this.page
+        .getByRole("button", { name: "Accepter alle" })
+        .first();
+      if (await cookieBanner.isVisible()) await cookieBanner.click();
+
+      const modalClose = this.page
+        .locator("button", { hasText: "Close" })
+        .first();
+      if (await modalClose.isVisible()) await modalClose.click();
+
+      const renewConsent = this.page.locator("button", {
+        hasText: "renew consent",
+      });
+      if (await renewConsent.isVisible()) await renewConsent.click();
+    };
+
+    // Retry dismissing overlays until stable
+    for (let i = 0; i < 3; i++) {
+      await dismissOverlay();
+      await this.page.waitForTimeout(500);
+    }
+  }
+
+  async goto() {
+    await this.page.goto("https://onskeskyen.dk/", {
+      waitUntil: "domcontentloaded",
+    });
+
+    // Click login up button
+    const loginButton = this.page.locator("button", { hasText: "Log ind" }); // this.page.locator('div.PublicMenu__LoginButtonWrapper-sc-56d3b660-2 button.Button__Container-sc-74e86c1a-0')
+    await loginButton.waitFor({ state: "visible", timeout: 15000 });
+    await loginButton.click();
+    await this.handlecokkies();
+
+    const container = this.page.locator(
+      "div.CustomContainer__Container-sc-8ccd59a3-0"
+    );
+    const continueWithEmail = container.locator("div", {
+      hasText: "Fortsæt med e-mail",
+    });
+    await continueWithEmail.click();
+
+    await this.handlecokkies();
+    const formContainer = this.page.locator("div", {
+      hasText: "Fortsæt med e-mail",
+    });
+    const pageTitle = formContainer.locator(
+      "div.RegisterStepTitle__PageTitle-sc-a498cb37-0"
+    );
+    await expect(pageTitle).toHaveText("Fortsæt med e-mail");
+
+    //await this.handleCookies();
+  }
+  async fillEmail(email: string) {
+    await this.emailInput.fill(email);
+    await this.handlecokkies();
+  }
+
+  // Fill password
+  async fillPassword(password: string) {
+    await this.passwordInput.fill(password);
+    await this.handlecokkies();
+  }
+
+  // Click login
+  async submit() {
+    await this.loginButton.click({ force: true });
+    await this.handlecokkies();
+    /*const cookieBanner = this.page.getByRole('button', { name: 'Accepter alle' }).first();
+        if (await cookieBanner.isVisible()) {
+            await cookieBanner.click();
+        }*/
+  }
+
+  async assertUserName(expectedName: string) {
+    // Dismiss cookie banner if visible
+    await this.handlecokkies();
+
+    const userNameLocator = this.page.locator(
+      "div.UserHeader__UserActionsContainer-sc-6440e44-5 div.UserHeader__Name-sc-6440e44-2"
+    );
+    await expect(userNameLocator).toBeVisible({ timeout: 15000 });
+
+    // Assert username text
+    await expect(userNameLocator).toHaveText(expectedName);
+  }
+}
